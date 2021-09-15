4fa2bda29c92d615efe65572be3c1c414b2420bf
Sydney <sydneyparsonsyb@gmail.com>
2021-09-14 22:10:51 -0400

    model setups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
-    validates :name, presence: true
+    validates :name, :age, presence: true
     validates :password_digest, :session_token, presence: true
     validates :password, length: { minimum: 6 }, allow_nil: true
 
