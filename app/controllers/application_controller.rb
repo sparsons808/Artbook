9947637deed01d3b2aba90ee6aa67a6cebce836f
Sydney <sydneyparsonsyb@gmail.com>
2021-09-15 16:25:10 -0400

    app and session controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+
+    def login!(user)
+        session[:session_token] = user.session_token
+    end
+
+    def logout!
+        current_user.reset_session_token!
+
+        session[:session_token] = nil
+    end
+
+    def current_user
+        return nil unless session[:session_token]
+
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def enssure_logged_out
+        redircted_to "/show/#{@user.id}" if logged_in?
+    end
+
+    def enssure_logged_in
+        redircted_to '/' unless logged_in?
+    end
 end
