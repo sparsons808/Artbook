1a8912f3c2872343fc7027745dd9cf9d68596a8d
Sydney <sydneyparsonsyb@gmail.com>
2021-09-23 14:12:55 -0400

    friends

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -1,3 +1,12 @@
 class Api::FriendsController < ApplicationController
+    def index
+        @friends = Friend.all.where(:user_id => params[:id])
+        render :index
+    end
 
+    private
+
+    def friend_pramas
+        params.require.(:friend).permit(:user_id, :friend_id)
+    end
 end
