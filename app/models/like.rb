fa06163e15bcb413b0d08ac2f28dd97992f97aae
sparsons808 <83877102+sparsons808@users.noreply.github.com>
2022-01-12 17:05:21 -0500

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -10,21 +10,8 @@
 #  notable_id   :bigint
 #
 class Like < ApplicationRecord
-
+    
     belongs_to :notable, polymorphic: true
     
-    belongs_to :liker,
-    primary_key: :id,
-    foreign_key: :liked_id,
-    class_name: :User
-
-    belongs_to :liked_post,
-    primary_key: :id,
-    foreign_key: :liker_id,
-    class_name: :Post
-
-    belongs_to :liked_comment,
-    primary_key: :id,
-    foreign_key: :liker_id,
-    class_name: :Comment
+    
 end
