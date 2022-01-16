248e5037c5b2296a2ad946557bcd2c32915c0b2a
sparsons808 <83877102+sparsons808@users.noreply.github.com>
2022-01-15 20:28:30 -0500

    Update likes_container.js

--- frontend/components/likes/likes_container.js
+++ frontend/components/likes/likes_container.js
@@ -1,11 +1,13 @@
 import Likes from "./likes";
 import { connect } from "react-redux";
-
+import { addLike, deleteLike, getLikes } from "../../actions/likes_actions";
 
 const mSTP = (state) => ({
     likes: state.entities.likes
 })
 
 const mDTP = (dispatch) => ({
-
+    addLike: like => dispatch(addLike(like)),
+    deleteLike: likeId => dispatch(deleteLike(likeId)),
 })
+
